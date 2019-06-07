7828a53fb554dacd2b8af7c48fc8b0082fa26c83
theAsteriks <stefan.dimov@student.kuleuven.be>
2019-06-07 15:20:29 +0200

    crypto_problems

--- crypt.py
+++ crypt.py
@@ -4,6 +4,7 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.kbkdf import \
 (CounterLocation, KBKDFHMAC, Mode)
+import cryptography.hazmat.backends
 import base64
 
 class Disenchant(object):
@@ -30,7 +31,7 @@ class Disenchant(object):
     label=label,\
     context=context,\
     fixed=None,\
-    backend=cryptography.hazmat.backends.default_backend())
+    backend=backends.default_backend())
     return base64.urlsafe_b64encode(kdf.derive(self.__getSerialNum(config.SER_N_FILE_LOC)))
   
   def getDBdata(self):
